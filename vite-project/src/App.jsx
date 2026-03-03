874ad2fbaf54966b1b5f493123f54c8a0171aaec
sabiha-md <sabihabegum78695@gmail.com>
2026-03-03 09:48:54 +0530

    Initial commit - Counter App

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter  from "./component/counter";
+
+const App = () => {
+  return (
+    <div className="page-container">
+      <div className="counter-card">
+        <Counter />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
